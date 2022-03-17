ec2b1f11e0beeef4ceef0c542885a3d28970a3c5
SIAMION DANKO <siadan@alum.us.es>
2022-03-17 21:57:08 +0100

    Fixing the queries for the dashboard

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -8,36 +8,36 @@ import acme.framework.repositories.AbstractRepository;
 @Repository
 public interface AdministratorDashboardRepository extends AbstractRepository{
 	
-	@Query("SELECT count(c) FROM Component c WHERE c.typeEntity ='COMPONENT'")
+	@Query("SELECT count(c) FROM Component c")
 	Integer totalNumberOfComponents();
 	
-	@Query("SELECT avg(c.retailPrice.amount) FROM Component c WHERE c.typeEntity ='COMPONENT' GROUP BY c.technology and c.retailPrice.currency")
+	@Query("SELECT avg(c.retailPrice.amount) FROM Component c GROUP BY c.technology and c.retailPrice.currency")
 	Double averageRetailPriceOfComponents();
 	
-	@Query("SELECT stddev(c.retailPrice.amount) FROM Component c WHERE c.typeEntity ='COMPONENT' GROUP BY c.technology and c.retailPrice.currency")
+	@Query("SELECT stddev(c.retailPrice.amount) FROM Component c GROUP BY c.technology and c.retailPrice.currency")
 	Double deviationRetailPriceOfComponents();
 	
-	@Query("SELECT min(c.retailPrice.amount) FROM Component c WHERE c.typeEntity ='COMPONENT' GROUP BY c.technology and c.retailPrice.currency")
+	@Query("SELECT min(c.retailPrice.amount) FROM Component c GROUP BY c.technology and c.retailPrice.currency")
 	Double minimumRetailPriceOfComponents();
 	
-	@Query("SELECT max(c.retailPrice.amount) FROM Component c WHERE c.typeEntity ='COMPONENT' GROUP BY c.technology and c.retailPrice.currency")
+	@Query("SELECT max(c.retailPrice.amount) FROM Component c GROUP BY c.technology and c.retailPrice.currency")
 	Double maximumRetailPriceOfComponents();
 	
 	////
 	
-	@Query("SELECT count(t) FROM Tool t WHERE t.typeEntity ='TOOL'")
+	@Query("SELECT count(t) FROM Tool t")
 	Integer totalNumberOfTools();
 	
-	@Query("SELECT avg(t.retailPrice.amount) FROM Tool t WHERE t.typeEntity ='TOOL' GROUP BY t.retailPrice.currency")
+	@Query("SELECT avg(t.retailPrice.amount) FROM Tool t GROUP BY t.retailPrice.currency")
 	Double averageRetailPriceOfTools();
 	
-	@Query("SELECT stddev(t.retailPrice.amount) FROM Tool t WHERE t.typeEntity ='TOOL' GROUP BY t.retailPrice.currency")
+	@Query("SELECT stddev(t.retailPrice.amount) FROM Tool t GROUP BY t.retailPrice.currency")
 	Double deviationRetailPriceOfTools();
 	
-	@Query("SELECT min(t.retailPrice.amount) FROM Tool t WHERE t.typeEntity ='TOOL' GROUP BY t.retailPrice.currency")
+	@Query("SELECT min(t.retailPrice.amount) FROM Tool t GROUP BY t.retailPrice.currency")
 	Double minimumRetailPriceOfTools();
 	
-	@Query("SELECT max(t.retailPrice.amount) FROM Tool t WHERE t.typeEntity ='TOOL' GROUP BY t.retailPrice.currency")
+	@Query("SELECT max(t.retailPrice.amount) FROM Tool t GROUP BY t.retailPrice.currency")
 	Double maximumRetailPriceOfTools();
 
 }
