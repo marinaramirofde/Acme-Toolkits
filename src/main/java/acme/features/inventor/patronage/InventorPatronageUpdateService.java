6bd799dbf8a6bbde706bcf89e567843016dc4fa7
marinaramirofde <marinaramirofde@gmail.com>
2022-04-27 11:19:37 +0200

    Task 005: update problem, almost done

---
+++ src/main/java/acme/features/inventor/patronage/InventorPatronageUpdateService.java
@@ -0,0 +1,97 @@
+package acme.features.inventor.patronage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.patronages.Patronage;
+import acme.entities.patronages.PatronageStatus;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Errors;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractUpdateService;
+import acme.roles.Inventor;
+
+@Service
+public class InventorPatronageUpdateService implements AbstractUpdateService<Inventor, Patronage> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected InventorPatronageRepository repository;
+
+	// AbstractUpdateService<Inventor, Patronage> interface -----------------
+
+
+	@Override
+	public boolean authorise(final Request<Patronage> request) {
+		assert request != null;
+
+		boolean result;
+		int patronageId;
+		Patronage patronage;
+
+		patronageId = request.getModel().getInteger("id");
+		patronage = this.repository.findOnePatronageById(patronageId);
+		
+		result = patronage != null && 
+			patronage.getStatus().equals(PatronageStatus.PROPOSED) && 
+			request.isPrincipal(patronage.getInventor());
+
+		return result;
+	}
+
+	@Override
+	public Patronage findOne(final Request<Patronage> request) {
+		assert request != null;
+
+		Patronage result;
+		int id;
+
+		id = request.getModel().getInteger("id");
+		result = this.repository.findOnePatronageById(id);
+
+		return result;
+	}
+
+	@Override
+	public void bind(final Request<Patronage> request, final Patronage entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+
+		request.bind(entity, errors, "budget", "code", "creationMoment", "endMoment", "info", 
+			"legalStuff","startMoment","status");
+	}
+
+	@Override
+	public void unbind(final Request<Patronage> request, final Patronage entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		request.unbind(entity, model, "budget", "code", "creationMoment", "endMoment", "info", 
+			"legalStuff","startMoment","status");
+		
+		model.setAttribute("inventorId", entity.getInventor().getId());
+	}
+
+	@Override
+	public void validate(final Request<Patronage> request, final Patronage entity, final Errors errors) {
+		assert request != null;
+		assert entity != null;
+		assert errors != null;
+		
+		// AÑADIR RESTRICCIÓN DE LA FECHA ---------------------------------------------------------------
+		// 	endMoment << startMoment << creationMoment;
+
+	}
+
+	@Override
+	public void update(final Request<Patronage> request, final Patronage entity) {
+		assert request != null;
+		assert entity != null;
+
+		this.repository.save(entity);
+	}
+
+}
