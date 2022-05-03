760255271a72256d367d5c23456c77db1fb6bd15
anglorcas <anglorcas@alum.us.es>
2022-05-03 12:44:06 +0200

    Task 009: done

--- src/main/java/acme/features/administrator/systemConfiguration/AdministratorSystemConfigurationController.java
+++ src/main/java/acme/features/administrator/systemConfiguration/AdministratorSystemConfigurationController.java
@@ -15,8 +15,12 @@ public class AdministratorSystemConfigurationController extends AbstractControll
 	@Autowired
 	protected AdministratorSystemConfigurationShowService showService;
 	
+	@Autowired
+	protected AdministratorSystemConfigurationUpdateService updateService;
+	
 	@PostConstruct
 	protected void initialise() {
 		super.addCommand("show", this.showService);
+		super.addCommand("update", this.updateService);
 	}
 }
